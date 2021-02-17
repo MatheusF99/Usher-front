b8f1ddb349e8bb1f757474e4863f896308d40853
matheusF99 <mc79846@gmail.com>
2021-02-17 17:15:26 -0300

    project get values from API

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import api from './services/api';
 
+
+interface User{
+  id: string,
+  name: string;
+  email: string;
+}
 
 function App() {
+
+  const [users, setUsers] = useState<User[]>([])
+  
+  useEffect(() => {
+    api.get('/users').then(response =>{
+      //console.log(response.data);
+      setUsers(response.data)
+    })
+  }, [])
+  
+
   return (
     <div className="App">
       <h1>hello world</h1>
+      {
+        users.map(user =>{
+          return(
+            <p key={user.id}> name: {user.name}, email: {user.email}</p>
+          )
+        })
+      }
     </div>
   );
 }
